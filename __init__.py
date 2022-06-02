7309fb1ae1890dd04ae734b6f1beae78b00fae1b
yortabortsgv <106741949+yortabortsgv@users.noreply.github.com>
2022-06-02 12:48:48 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,4 +7,3 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 here = path.abspath(path.dirname(__file__))
 # Loading version number
 with open(path.join(here, 'VERSION')) as version_file:
-    __version__ = version_file.read().strip()
